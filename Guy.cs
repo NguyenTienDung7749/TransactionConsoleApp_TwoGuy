7f480c25c3190d6fa05c3cec8eb6dbfbe9019710
NguyenTienDung7749 <toinangpham@gmail.com>
2025-04-22 08:32:45 +0700

    Add project files.

---
+++ Guy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsoleApp
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+        /// <summary>
+        /// Viết Tên và Số Tiền của đối tượng đang có
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"{Name} has {Cash} bucks.");
+        }
+        /// <summary>
+        /// Chuyển tiền từ ví đối tượng sang người nhận ( hoặc hiện
+        /// thông báo nếu không đủ tiền)
+        /// </summary>
+        /// <param name="amount">Số Tiền Cho Vay=</param>
+        /// <returns>
+        /// Số tiền cho vay thành công hoặc 0 nếu không đủ tiền
+        /// </returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't a valid amount dzungf ");
+                    return -1;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount}");
+                    return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+        /// <summary>
+        /// Nhận tiền vào ví của đối tượng (hoặc hiện thông báo nếu số tiền không hợp lệ)
+        /// </summary>
+        /// <param name="amount">Số Tiền Được Giải Ngân</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine($"{Name} says: are you kidding me dzungf ");
+                return;
+            }
+            if (amount == 0)
+            {
+                Console.WriteLine($"{Name} says: you are so poor");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Transaction Complete Successfully");
+                Console.WriteLine($"{Name} says: I have received {amount} bucks");
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace PracticeConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy joe = new() { Name = "Joe", Cash = 50 };
+            Guy bob = new() { Name = "Bob", Cash = 100 };
+            while (true)
+            {   
+                Console.WriteLine("Hello Bob and Joe I am dzungf and will be the witness to your transaction.");
+                joe.WriteMyInfo();
+                bob.WriteMyInfo();
+                Console.Write("Enter an amount you want to give (or a blank to exit): ");
+                string howMuch = Console.ReadLine();
+                if (howMuch == "") return;
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    Console.Write("Who should give the cash (Joe or Bob) : ");
+                    
+                    string whichGuy = Console.ReadLine();
+                    while (whichGuy != "Joe" && whichGuy != "Bob")
+                    {
+                        Console.Write("Please enter either Joe or Bob: ");
+                        whichGuy = Console.ReadLine();
+                    }
+                    if (whichGuy == "Joe")
+                    {
+                        int cashGiven = joe.GiveCash(amount);
+                        bob.ReceiveCash(cashGiven);
+                        Console.WriteLine("Enter to start new transaction");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else if (whichGuy == "Bob")
+                    {
+                        int cashGiven = bob.GiveCash(amount);
+                        joe.ReceiveCash(cashGiven);
+                        Console.WriteLine("Enter to start new transaction");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Plase enter either Joe or Bob");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an amount (or a blank to exit).");
+                }
+
+            }
+        }
+    }
+}
